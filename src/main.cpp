46100e56042dc69e61599b975b2dda9e4cbdec45
littlemight <mfang555@gmail.com>
2020-01-22 02:04:07 +0700

    fix bug kalo n = 1

--- src/main.cpp
+++ src/main.cpp
@@ -113,8 +113,8 @@ void findHull(bool findMinPoints) {
     map<LINE, pair<int, int>> lineCheck; // menyimpan dua titik terjauh untuk sebuah garis
     vector<bool> vis(n, 0);
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
             refLine = makeLine(pts[i], pts[j]);
             bool left = false, right = false;
             bool can = true;
